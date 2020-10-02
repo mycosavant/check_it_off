de31b97bf63c6e40ce1827bd173097a2908e8446
ppb1701 <ppb1701@gmail.com>
2020-10-02 02:45:14 -0400

    adding calendar plugin to research integration

---
+++ lib/helpers/calendar.dart
@@ -0,0 +1,21 @@
+import 'package:add_2_calendar/add_2_calendar.dart';
+
+class CalendarHelper{
+  CalendarHelper();
+
+  void addTaskToCalendar(Event e){
+    Add2Calendar.addEvent2Cal(e);
+  }
+
+  Event addEventForCalendar(String title, String description, DateTime startDate, DateTime endDate){
+    return Event(
+      title: title,
+      description: description,
+      location: '',
+      startDate: startDate,
+      endDate: endDate,
+      timeZone: '',
+      allDay: true
+    );
+  }
+}
