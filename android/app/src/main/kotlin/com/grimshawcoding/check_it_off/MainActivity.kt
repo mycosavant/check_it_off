92242d16078493d4e31ed35845535629fb4f2344
ppb1701 <ppb1701@gmail.com>
2020-12-03 04:18:22 -0500

    upgraded Android API encoding.

--- android/app/src/main/kotlin/com/grimshawcoding/check_it_off/MainActivity.kt
+++ android/app/src/main/kotlin/com/grimshawcoding/check_it_off/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.grimshawcoding.check_it_off
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity;
 
 class MainActivity: FlutterActivity() {
 }
